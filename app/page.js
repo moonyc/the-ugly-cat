3dd3b474958177733e1e44db65648e7b95a9cafe
muni <booscarface@gmail.com>
2023-06-02 22:06:29 +0200

    popular videos api

--- app/page.js
+++ app/page.js
@@ -1,36 +1,21 @@
-import Image from 'next/image'
+
 import styles from './page.module.css'
 import Banner from '@/app/components/banner'
 import NavBar from './components/nav/navbar'
 import SectionCards from './components/card/section-cards'
-// import { getVideos } from '../lib/videos'
-
-async function getData() {
-  
-  const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${YOUTUBE_API_KEY}`)
-  
-  if (!response.ok) {
-    throw new Error('Failed to fetch data')
-  } else {
-    const data = await response.json()
-    return data.items.map((item) => {
-      return {
-          title: item.snippet.title,
-          imgUrl: item.snippet.thumbnails.high.url,
-          id: item?.id?.videoId,
-      }
-  })
-  }
-}
-
+import { getPopularVideos, getVideos } from '../lib/videos'
 
 
 
 export  default async function Home() {
-  const disneyVideos = await getData()
+  const historyVideos = await getVideos('alessandro barbero')
+  const newyorkVideos = await getVideos('new york city skyline shorts')
+  const johnnyharrisVideos = await getVideos('johnny harris videos')
+  const breakingItalyVideos= await getVideos('Breaking Italy Night')
+  const popularVideos = await getPopularVideos()
+
   return (
-    <div className={styles.container}>
+    <div className={styles.main}>
     
     <NavBar username="theuglycat@sashacorp.dev" />
       <Banner  
@@ -39,9 +24,11 @@ export  default async function Home() {
         imgUrl="/nyc.jpg"  
         />
         <div className={styles.sectionWrapper}>
-        <SectionCards title="Disney" videos={disneyVideos} size="large"/>
-        <SectionCards title="Disney" videos={disneyVideos} size="medium"/>
-        <SectionCards title="Disney" videos={disneyVideos} size="small"/>
+        <SectionCards title="New York" videos={newyorkVideos} size="large"/>
+        <SectionCards title="History" videos={historyVideos} size="small"/>
+        <SectionCards title="US Journalism" videos={johnnyharrisVideos} size="small"/>
+        <SectionCards title="Politics" videos={breakingItalyVideos} size="small"/>
+        {/* <SectionCards title="Popular" videos={popularVideos} size="small"/> */}
         </div>
     </div>
   )
