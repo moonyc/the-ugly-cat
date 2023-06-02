295e29d9dc17c479320f91d2b6d6da15985e6168
muni <booscarface@gmail.com>
2023-06-02 17:03:22 +0200

    data fetching

--- app/page.js
+++ app/page.js
@@ -3,11 +3,32 @@ import styles from './page.module.css'
 import Banner from '@/app/components/banner'
 import NavBar from './components/nav/navbar'
 import SectionCards from './components/card/section-cards'
-import { getVideos } from '../lib/videos'
+// import { getVideos } from '../lib/videos'
+
+async function getData() {
+  
+  const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${YOUTUBE_API_KEY}`)
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch data')
+  } else {
+    const data = await response.json()
+    return data.items.map((item) => {
+      return {
+          title: item.snippet.title,
+          imgUrl: item.snippet.thumbnails.high.url,
+          id: item?.id?.videoId,
+      }
+  })
+  }
+}
 
 
-export  default function Home() {
-  const disneyVideos = getVideos()
+
+
+export  default async function Home() {
+  const disneyVideos = await getData()
   return (
     <div className={styles.container}>
     
